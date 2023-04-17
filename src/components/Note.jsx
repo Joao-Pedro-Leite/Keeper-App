8139a8b3e49326d62ee569b9699701f1f138fc5f
João Pedro Leite <leite.jpedro@gmail.com>
2023-04-17 09:59:11 -0300

    Terminado

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+
 function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={() => props.deleteCard(props.id)}>DELETE</button>
+      <button id="lixeira" onClick={handleClick}>
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
